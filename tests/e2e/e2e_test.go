c131d28b3e1e84c94353d6a1f5248b37f4d1d344
Sam Caldwell <github@samcaldwell.net>
2026-07-04 02:59:37 +0000

    fix(ci): use protobuf encoding in E2E test (JSON omits TraceID/SpanID)

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -44,11 +44,11 @@ func TestFullPipeline(t *testing.T) {
 	defer srv.Close()
 	addr := ln.Addr().String()
 
-	// Send 50 traces
+	// Send 50 traces via protobuf (JSON omits TraceID/SpanID due to json:"-" tags)
 	for i := byte(1); i <= 50; i++ {
 		td := makeTD(i)
-		body, _ := json.Marshal(td)
-		resp, err := http.Post("http://"+addr+"/v1/traces", "application/json", bytes.NewReader(body))
+		body, _ := otlp.Marshal(td)
+		resp, err := http.Post("http://"+addr+"/v1/traces", "application/x-protobuf", bytes.NewReader(body))
 		if err != nil {
 			t.Fatal(err)
 		}
